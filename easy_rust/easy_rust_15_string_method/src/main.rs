bf06d87c662742213c551cb25207690af819da0c
hex-aragon <0xaragon90@gmail.com>
2025-03-12 02:33:00 +0900

    ADD: string, string method

--- easy_rust/easy_rust_15_string_method/src/main.rs
+++ easy_rust/easy_rust_15_string_method/src/main.rs
@@ -1,15 +1,34 @@
 fn main() {
-    let mut v: Vec<i32> = Vec::new();
-    v.push(1);
-    v.push(2);
-    v.push(3);
+    println!("Hello World!");
+    // allocation   어떤 변수에 16바이트 할당 (ex)
+    // reallocation 다시 같은 변수에 32바이트 재할당  (ex)
+        
+        
+    //String
+    //.capacity
+    //.push > 1글자 
+    //.push_str > 1글자 이상
+    //.pop
+    //.with_capacity
+    //allocation
+    //.function .으로 쓰는 걸 메소드라고 함
+    //len는 capacity보다 작거나 같다. 
     
-    // 또는 아래와 같이 vec! 매크로를 이용하여 초기화할 수 있습니다.
-    let v2 = vec![1, 2, 3];
-    println!("{:?}",v2); //있으면 좋을 것 같아요 출력 변수 
-
-
+    //let mut my_name = "".to_string();
+    let mut my_name = String::with_capacity(26);
+    println!("Length is : {} , Capacity is :{}", my_name.len(), my_name.capacity());
     
+    my_name.push_str("David");
+    println!("Length is : {} , Capacity is :{}", my_name.len(), my_name.capacity());
+    
+    my_name.push('!');
+    println!("Length is : {} , Capacity is :{}", my_name.len(), my_name.capacity());
+    
+    my_name.push_str("and i live in seoul");
+    println!("Length is : {} , Capacity is :{}", my_name.len(), my_name.capacity());
+    
+    my_name.push('a');
+    println!("Length is : {} , Capacity is :{}", my_name.len(), my_name.capacity());
+        
+    println!("My name is {}", my_name); 
 }
-
-
