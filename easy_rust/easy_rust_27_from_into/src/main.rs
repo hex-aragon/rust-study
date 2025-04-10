efe3f3892ee5abfd6ab92be2d4e3b1a42c2612f8
hex-aragon <0xaragon90@gmail.com>
2025-04-11 06:55:41 +0900

    ADD: from, into

---
+++ easy_rust/easy_rust_27_from_into/src/main.rs
@@ -0,0 +1,21 @@
+// trait = 초능력 
+// trait를 만드는 방법 from, into 
+// This type implements (trait name)
+// 
+
+// From, Into
+
+ 
+fn main() {
+    
+    
+    let my_name = String::from("Dave MacLeod"); //from 메소드면서 trait? 
+    
+    let my_city: String = "Seoul".into(); // &str 
+ 
+    //my_city.asdfas();
+    println!("{}", my_city);
+    
+    let my_vec = Vec::from([8,9,10]); // [i32; 3]
+    println!("{:?}",my_vec);
+}
