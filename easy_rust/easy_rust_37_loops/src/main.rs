ce994e93fb60a45f2f3043866e55963cf85a39ad
hex-aragon <0xaragon90@gmail.com>
2025-04-12 19:37:44 +0900

    ADD: loops

---
+++ easy_rust/easy_rust_37_loops/src/main.rs
@@ -0,0 +1,26 @@
+//loops 
+
+fn main() {
+
+    let mut counter = 0;
+    let mut counter2 = 0;
+    
+   'first_loop: loop {
+       counter +=1;
+       println!("The counter is now : {} ", counter);
+       if counter > 9 {
+           println!("Now entering the second loop");
+           
+           'seoncd_loop: loop {
+            
+                println!("The second counter is: {}", counter2);
+                counter2 += 1;    
+                if counter2 == 3 {
+                    break 'first_loop;
+                }
+            }    
+       }
+    }
+    
+
+}
