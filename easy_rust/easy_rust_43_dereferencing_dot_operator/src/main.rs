04f18bfcfd18dd1b47b85da49618b39abb14a752
hex-aragon <0xaragon90@gmail.com>
2025-04-16 05:57:51 +0900

    Edit change folder name

--- easy_rust/easy_rust_43_dereferencing_dot_operator/src/main.rs
+++ easy_rust/easy_rust_43_dereferencing_dot_operator/src/main.rs
@@ -1,3 +1,40 @@
+// references and the dot operator
+// .
+// . 
+
+struct Item {
+    number: u8
+}
+
+// . dot operator
+impl Item {
+    fn compare_number(&self, other_number: u8){
+        println!("Are they equal? {}", self.number == other_number);
+    }
+}
+
+// Deref * 
 fn main() {
-    println!("Hello, world!");
+    let item = Item {
+        number: 10
+    };
+
+    //let my_number = 10;  // i32 
+    // let reference = &my_number;  // &i32 
+    
+    // //ref 변수에 * 붙여서 dereference
+    // println!("Are they the same? {}", my_number == *reference);
+    
+    // let reference = &item.number; //&u8
+    // //reference.thsadf(); 타입 체크 ?  
+    // println!("{}", reference == 10u8);
+    
+    //https://doc.rust-lang.org/nomicon/ == 러스트의 비결?, 구조 ?에대한 책  
+    
+    let reference_item = &item;
+    item.compare_number(10);
+    let other_reference_item = &reference_item; // &&Item
+    
+    reference_item.compare_number(10); //&Item
+    other_reference_item.compare_number(10); 
 }
